6290f7eac56543f2e6a3bb22d0fd721f09035315
roman <laisk1@hotmail.com>
2019-04-14 12:25:49 +0300

    Blockchain Skeleton

---
+++ blockchain.py
@@ -0,0 +1,30 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transaction = []
+
+
+
+
+    def new_blck(self):
+        #creation new block and adding
+        pass 
+
+
+
+    def new_transaction(self):
+        #adding new transaction in list
+        pass
+
+
+    @hashing
+    def hash(self):
+        #hashing block 
+        pass
+
+
+    @property
+    def last_block(self):
+        #returning the last block of chain
+        pass
+
